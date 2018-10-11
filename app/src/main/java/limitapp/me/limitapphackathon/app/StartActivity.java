3e390d690967bca3ebe6b65c501541b77c82b4fd
Stevan Bogosavljevic <stevyhacker@gmail.com>
2018-10-11 08:36:00 +0200

    Created navigation tabs

---
+++ app/src/main/java/limitapp/me/limitapphackathon/app/StartActivity.java
@@ -0,0 +1,27 @@
+package limitapp.me.limitapphackathon.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import limitapp.me.limitapphackathon.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+}
