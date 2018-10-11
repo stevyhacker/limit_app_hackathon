1010facf93ef6f8ce981f64f1f9165b32e5c0903
Stevan Bogosavljevic <stevyhacker@gmail.com>
2018-10-11 11:31:18 +0200

    Start screen design completed

--- app/src/main/java/limitapp/me/limitapphackathon/app/StartActivity.java
+++ app/src/main/java/limitapp/me/limitapphackathon/app/StartActivity.java
@@ -1,21 +1,23 @@
 package limitapp.me.limitapphackathon.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import limitapp.me.limitapphackathon.R;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_start);
 
         Button startBtn = findViewById(R.id.startBtn);
+
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
